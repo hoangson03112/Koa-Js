566086cd979f8040977d9b9c6a8ec57dd8df185f
hoangson03112 <rtwf0311@gmail.com>
2023-12-18 21:52:52 +0700

    koa-js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-const Koa = require('koa');
-const koaBody = require('koa-body');
-const routes = require('./routes/routes.js');
-const render = require('koa-ejs');
-const path = require('path');
+const Koa = require("koa");
+const koaBody = require("koa-body");
+const routes = require("./routes/routes.js");
+const render = require("koa-ejs");
+const path = require("path");
 const app = new Koa();
-
 
 app.use(koaBody());
 app.use(routes.routes());
